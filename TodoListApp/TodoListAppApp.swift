8fa6abb463e45885e6b4bac693b8759c7ffc085f
Aver Alexey <68560245+cosmograve@users.noreply.github.com>
2025-02-21 12:41:30 +0700

    Initial Commit

---
+++ TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by Алексей on 21.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
